08f2ab1aa3149a924ce66d1da09733c5bf6617b8
Chris Hale <cghale@uh.edu>
2012-10-04 23:12:05 -0500

    First draft of create table scripts
    I have also attached a notes file of questions we should address as a
    team or as devs. One of them pertains to the logic behind the Likes
    table, and we should probably consult Nick on this one.

---
+++ CREATE_TABLE/CREATEscripts.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Users (
+         User_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         User_Email VARCHAR(50),
+		 User_Password VARCHAR(15)
+       );
+
+CREATE TABLE Events (
+         Event_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         Event_Type VARCHAR(50),
+		 Event_Name varchar(25),
+		 Event_Date date,
+		 Event_Time varchar(10)
+		 Event_Description varchar(100),
+		 Event_Contact_Info varchar(50)		
+       );
+	   
+CREATE TABLE Likes (
+         Likes_ID int,
+		 Likes_Type varchar(50),		 
+         Likes_Count int,
+		 FOREIGN KEY (Likes_ID) REFERENCES Users(User_ID),
+		 FOREIGN KEY (Likes_Type) REFERENCES Events(Event_ID)	
+       );
+
+	   
+CREATE TABLE Organizations (
+         Org_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         Org_Name varchar(50),
+		 Org_Website varchar(100),
+		 Org_Requirements varchar(100),
+		 Org_Mission varchar(100),
+		 Org_Num_Of_Ratings int,
+		 Org_Rating_Total int,
+		 Org_Description varchar(100),
+		 Org_Contact_Info varchar(50)		
+       );	   
