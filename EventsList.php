31966a1667eb643d9341876cd0ae8688e194f42f
Chris Hale <cghale@uh.edu>
2012-10-15 11:16:19 -0500

    EventsList fixes

--- EventsList.php
+++ EventsList.php
@@ -4,25 +4,23 @@
 	$myusername=$_SESSION['login_user'];
 	
 	
-	$sql="SELECT * FROM Events";
+	$sql="SELECT * FROM Events";   //STILL NEEDS DATE LOGIC AND FEATURED LOGIC
 	$result=mysql_query($sql);
 	
-	$num=mysql_numrows($result);
-
-	echo "<b><center>Database Output</center></b><br><br>";
+	$num=mysql_numrows($result);//GRABS NUMBER OF ROWS
 
 	$i=0;
-	while ($i < $num) 
+	while ($i < $num) //GETS CURRENT ROW IN TABLE AND MAKES SURE WE GO THROUGH ALL RECORDS
 	{
-		$eventID = mysql_result($result, $i, "Event_ID");
+		$eventID = mysql_result($result, $i, "Event_ID");  //GRABS THE DATA IN "COLUMN _NAME"(this is the title of the column) FROM CURRENT ROW FROM CURRENT QUERY
 		$eventName=mysql_result($result,$i,"Event_Name");
 		$eventDate=mysql_result($result,$i,"Event_Date");
 		$eventDesc=mysql_result($result,$i,"Event_Description");
 		$eventType=mysql_result($result,$i,"Event_Type");
 
-		echo "<b>$eventName"."<br>"."$eventDate</b><br>$eventDesc<br>$eventType<hr><br>";
+		echo "<b>$eventName"."<br>"."$eventDate</b><br>$eventDesc<br>$eventType<hr><br>"; //WE CAN FORMAT THIS TO PRINT AS PRETTY AS WE WANT
 
-		$i++;
+		$i++; //GO TO NEXT ROW
 	}
 
 ?>
