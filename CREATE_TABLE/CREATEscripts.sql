c0f2d0a226cfba534c550a1ec63ab3b333c1d7e4
Chris Hale <cghale@uh.edu>
2012-10-15 09:48:52 -0500

    Sign up now works

--- CREATE_TABLE/CREATEscripts.sql
+++ CREATE_TABLE/CREATEscripts.sql
@@ -7,24 +7,35 @@ CREATE TABLE Users (
 
 CREATE TABLE Events (
          Event_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-         Event_Type VARCHAR(50),
 		 Event_Name varchar(25),
+		 Event_OrgName varchar(50),
 		 Event_Date date,
-		 Event_Time varchar(10)
+		 Event_DateAdded date,
 		 Event_Description varchar(100),
-		 Event_Contact_Info varchar(50)		
+		 Event_Type varchar(25),
+		 Event_StartTime varchar(10),
+		 Event_EndTime varchar(10),
+		 Event_ContactName varchar(50),
+		 Event_ContactPhone varchar(15),
+		 Event_ContactEmail varchar(50),
+		 Event_Location varchar(50),
+		 Event_Comments varchar(50)		
        );
 
 CREATE TABLE Organizations (
          Org_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
          Org_Name varchar(50),
-		 Org_Website varchar(100),
+		 Org_Website varchar(50),
+		 Org_Location varchar(50),
+		 Org_ContactName varchar(50),
+		 Org_ContactPhone varchar(15),
+		 Org_ContactEmail varchar(50),
 		 Org_Requirements varchar(100),
 		 Org_Mission varchar(100),
-		 Org_Num_Of_Ratings int,
-		 Org_Rating_Total int,
-		 Org_Description varchar(100),
-		 Org_Contact_Info varchar(50),
+		 Org_NumOfRatings int,
+		 Org_RatingTotal int,
 		 Org_UserEmail varchar(50),
-		 FOREIGN KEY (Org_UserEmail) REFERENCES Users(User_Email)
-       );		   
+		 Org_LeadCount int,
+		 Org_Type varchar(25),
+		 Org_JoinDate date
+       );	   
---
+++ SignUp.php
@@ -0,0 +1,210 @@
+<?php
+	include("config.php");  //Establishes DB connection
+
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		if($_POST['checkVal']=="true")			//Check to see if the Org checkbox has been selected
+		{
+			// Org infor from form is gathered here
+			$myorgname=addslashes($_POST['orgname']);
+			$mywebsite=addslashes($_POST['website']);
+			$mylocation=addslashes($_POST['orglocation']);
+			$mycontactname=addslashes($_POST['contactname']);
+			$mycontactphone=addslashes($_POST['contactphone']);
+			$mycontactemail=addslashes($_POST['contactemail']);
+			$myrequirements=addslashes($_POST['requirements']);
+			$mymission=addslashes($_POST['mission']);
+			$myorgtype=addslashes($_POST['orgtype']);
+			$myorgusername=addslashes($_POST['username']);
+				if(($myorgname != '' && $mywebsite != '' && $mycontactname != '') || ($myorgname !=null && $mywebsite != null && $mycontactname != null)) //Are all required fields filled out?
+				{
+
+					//Check to see if Org already exists
+					$sql="SELECT Org_Name FROM Organizations WHERE Org_Name='$myorgname'";
+					$result=mysql_query($sql);
+					$count=mysql_num_rows($result);
+
+					if($count>=1)
+					{
+						echo "This organization already exists already exists!";
+					}
+					else		//If not, insert into org table
+					{	
+						$sql2="INSERT INTO Organizations (
+						Org_Name,
+						Org_Website,
+						Org_Location,
+						Org_ContactName,
+						Org_ContactPhone,
+						Org_ContactEmail,
+						Org_Requirements,
+						Org_Mission,
+						Org_NumOfRatings,
+						Org_RatingTotal,
+						Org_UserEmail,
+						Org_LeadCount,
+						Org_Type,
+						Org_JoinDate) 
+							VALUES (
+						'$myorgname',
+						'$mywebsite',
+						'$mylocation',
+						'$mycontactname',
+						'$mycontactphone',
+						'$mycontactemail',
+						'$myrequirements',
+						'$mymission',
+						0,
+						0,
+						'$myorgusername',
+						0,
+						'$myorgtype',
+						'".date("Y-m-d")."')";
+						mysql_query($sql2);
+						
+		
+						echo "Thanks for signing up!!";
+		
+					}
+
+				}
+				else //If all required fields are not filled in, error message displayed
+				{
+					echo "Please fill out all required fields.";
+				}
+			//Gather user info from form
+			$myusername=addslashes($_POST['username']);
+			$mypassword=addslashes($_POST['password']);
+			$mypassword2=addslashes($_POST['password2']);
+				if(($myusername != '' && $mypassword != '' && $mypassword2 != '') || ($myusername !=null && $mypassword != null && $mypassword2 != null)) //Make sure info is filled in
+				{
+					if($mypassword != $mypassword2) //Do the passwords match? If not, try again....
+					{
+						echo "Your passwords do not match! Please try again.";
+					}
+					else		//If so, Make sure username doesn't already exist 
+					{
+						$sql="SELECT User_ID FROM Users WHERE User_Email='$myusername'";
+						$result=mysql_query($sql);
+						$count=mysql_num_rows($result);
+
+							if($count>=1)
+							{
+								echo "This username already exists!";
+							}
+							else //Insert into user table
+							{
+								$sql2="INSERT INTO Users (User_Email, User_Password, User_IsOrg) VALUES ('$myusername','$mypassword',true)";
+								mysql_query($sql2);
+		
+								echo "Thanks for signing up!!";
+		
+							}
+					}
+				}
+				else		//Enter information if it is not already filled out
+				{
+					echo "Please enter all information...";
+				}
+
+		}
+		else
+		{
+			//Gather user info from form
+			$myusername=addslashes($_POST['username']);
+			$mypassword=addslashes($_POST['password']);
+			$mypassword2=addslashes($_POST['password2']);
+				if(($myusername != '' && $mypassword != '' && $mypassword2 != '') || ($myusername !=null && $mypassword != null && $mypassword2 != null))
+				{
+					if($mypassword != $mypassword2)    //Do the passwords match? If not, try again....
+					{
+						echo "Your passwords do not match! Please try again.";
+					}
+					else		//If so, Make sure username doesn't already exist
+					{
+
+						$sql="SELECT User_ID FROM Users WHERE User_Email='$myusername'";
+						$result=mysql_query($sql);
+						$count=mysql_num_rows($result);
+
+							if($count>=1)		
+							{
+								echo "This username already exists!";
+							}
+							else			//Insert into user table
+							{
+								$sql2="INSERT INTO Users (User_Email, User_Password, User_IsOrg) VALUES ('$myusername','$mypassword',false)";
+								mysql_query($sql2);
+		
+								echo "Thanks for signing up!!";
+		
+							}
+					}
+				}
+				else		//Enter information if it is not already filled out
+				{
+					echo "Please enter all information...";
+				}
+		}
+	}
+
+?>
+<html>
+
+<script>
+function hiddenBoxes()
+{
+	var value = document.form1.elements[4].checked;
+	if(value)
+	{
+		for(x = 5; x< document.form1.length-1; x++)
+			document.form1.elements[x].hidden = false;
+		
+	}
+	else
+	{
+		for(x = 5; x< document.form1.length-1; x++)
+			document.form1.elements[x].hidden = true;
+	}
+	
+	document.form1.elements[0].value = document.form1.elements[4].checked;
+	
+}
+</script>
+
+<body>
+
+<form name="form1" action="" method="post">
+<input hidden name="checkVal" value="">
+<label>Enter your Email Address :</label>
+<input type="text" name="username"/><br />
+<label>Enter a Password :</label>
+<input type="password" name="password"/><br/>
+<label>Re-type your Password :</label>
+<input type="password" name="password2"  /><br/>
+<label>Is this an organization?</label>
+<input type="checkbox" name="org" value="yes" onclick="hiddenBoxes();" /><br />
+<label >*Organization Name :</label>
+<input type="text" name="orgname" hidden /><br />
+<label >*Organization Website :</label>
+<input hidden type="text" name="website"/><br/>
+<label >Organization Location :</label>
+<input type="text" name="orglocation" hidden /><br />
+<label >Contact Name :</label>
+<input type="text" name="contactname" hidden /><br />
+<label >Contact Phone :</label>
+<input type="text" name="contactphone" hidden /><br />
+<label >Contact Email :</label>
+<input type="text" name="contactemail" hidden /><br />
+<label >Organization Requirements :</label>
+<textarea hidden name="requirements"></textarea><br/>
+<label >Organization Mission :</label>
+<textarea  hidden name="mission"></textarea><br/>
+<label  >Organization Type :</label>
+<textarea  hidden name="orgtype"></textarea><br/>
+* - denotes required field<br />
+
+<input type="submit" value=" Submit "/><br />
+</form>
+</body>
+</html>
